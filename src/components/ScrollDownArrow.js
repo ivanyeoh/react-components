124eb273c4b019f373c33142b48f9590528de7fe
Ivan Yeoh <ivanbone@gmail.com>
2019-12-16 23:08:31 +0800

    react

--- src/components/ScrollDownArrow.js
+++ src/components/ScrollDownArrow.js
@@ -1,5 +1,7 @@
+import React from 'react'
+
 export default () => (
-  <div className="scroll-down-arrow">
+  <div className="scroll-down-arrow" title="scroll down">
     <span></span>
     <span></span>
     <span></span>
