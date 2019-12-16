bb71729924af8f1b8c52766df7006c833f2f65a0
Ivan Yeoh <ivanbone@gmail.com>
2019-12-17 00:07:50 +0800

    scrolled animation

--- src/components/ScrollDownArrow.js
+++ src/components/ScrollDownArrow.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default () => (
-  <div className="scroll-down-arrow" title="scroll down">
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
-)
+const scrollOver = (y, callback) => {
+  return () => {
+    if (document.documentElement.scrollTop > y) {
+      callback(true)
+    } else {
+      callback(false)
+    }
+  }
+}
+
+export default () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollOver(10, setScrolled))
+
+    return () => {
+      window.removeEventListener('scroll', scrollOver(10, setScrolled))
+    }
+  })
+
+  return (
+    <div
+      className={`scroll-down-arrow${scrolled ? ' scrolled' : ''}`}
+      title="scroll down">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+}
